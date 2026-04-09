7ff2b04043b4c6b276e7d9e20f203f44f935fdd8
Carlos Alberto Nin Queliz <128722564+Carlosnin0623@users.noreply.github.com>
2026-04-08 21:19:32 -0400

    Componentes

--- curso_angular/angular-peliculas/src/app/app.component.ts
+++ curso_angular/angular-peliculas/src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { CurrencyPipe, DatePipe, NgFor, UpperCasePipe } from '@angular/common';
+import { CurrencyPipe, DatePipe, NgFor, NgOptimizedImage, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 
 @Component({
   selector: 'app-root',
-  imports: [DatePipe, UpperCasePipe, CurrencyPipe, NgFor],
+  imports: [DatePipe, UpperCasePipe, CurrencyPipe, NgOptimizedImage],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -13,19 +13,44 @@ export class AppComponent {
   nombre = "Carlos Alberto"
   apellidos = "Nin Queliz";
   edad = 28;
-  duplicarNumero(valor: number) : number {
+  duplicarNumero(valor: number): number {
     return valor * 2
   };
-  peliculas = [
-    {
-      titulo: 'Spider-Man',
-      fechaLanzamiento : new Date(),
-      precio: 1400.99
-    },
-    {
-      titulo: 'Moana',
-      fechaLanzamiento : new Date("2016-05-03"),
-      precio: 300.99
-    },
-  ]
+  peliculas = [{
+    titulo: 'Inside Out 2',
+    fechaLanzamiento: new Date(),
+    precio: 1400.99,
+    poster: 'https://upload.wikimedia.org/wikipedia/en/f/f7/Inside_Out_2_poster.jpg?20240514232832'
+  },
+  {
+    titulo: 'Moana 2',
+    fechaLanzamiento: new Date('2016-05-03'),
+    precio: 300.99,
+    poster: 'https://upload.wikimedia.org/wikipedia/en/7/73/Moana_2_poster.jpg'
+  },
+  {
+    titulo: 'Bad Boys: Ride or Die',
+    fechaLanzamiento: new Date('2016-05-03'),
+    precio: 300.99,
+    poster: 'https://upload.wikimedia.org/wikipedia/en/8/8b/Bad_Boys_Ride_or_Die_%282024%29_poster.jpg'
+  },
+  {
+    titulo: 'Deadpool & Wolverine',
+    fechaLanzamiento: new Date('2016-05-03'),
+    precio: 300.99,
+    poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/4c/Deadpool_%26_Wolverine_poster.jpg/250px-Deadpool_%26_Wolverine_poster.jpg'
+  },
+  {
+    titulo: 'Oppenheimer',
+    fechaLanzamiento: new Date('2016-05-03'),
+    precio: 300.99,
+    poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/4a/Oppenheimer_%28film%29.jpg/250px-Oppenheimer_%28film%29.jpg'
+  },
+  {
+    titulo: 'The Flash',
+    fechaLanzamiento: new Date('2016-05-03'),
+    precio: 300.99,
+    poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/e/ed/The_Flash_%28film%29_poster.jpg/250px-The_Flash_%28film%29_poster.jpg'
+  }];
+
 }
