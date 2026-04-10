5ad63c1d8dcf4a737cad0f900e6712963167ed2d
Carlos Alberto Nin Queliz <128722564+Carlosnin0623@users.noreply.github.com>
2026-04-10 16:49:36 -0400

    Proyección de contenido

---
+++ curso_angular/angular-peliculas/src/app/peliculas/listado-peliculas/listado-peliculas.component.ts
@@ -0,0 +1,37 @@
+import { CurrencyPipe, DatePipe, NgOptimizedImage, UpperCasePipe } from '@angular/common';
+import { Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-listado-peliculas',
+  imports: [DatePipe, UpperCasePipe, CurrencyPipe, NgOptimizedImage],
+  templateUrl: './listado-peliculas.component.html',
+  styleUrl: './listado-peliculas.component.css'
+})
+export class ListadoPeliculasComponent { /* Con OnInit le decimos lo que tiene que hacer automaticamente se llame al componente  */
+
+  title = 'angular-peliculas';
+  nombre = "Carlos Alberto"
+  apellidos = "Nin Queliz";
+  edad = 28;
+  duplicarNumero(valor: number): number {
+    return valor * 2
+  };
+
+  @Input({required: true})
+  peliculas!: any[]; /* Con este simbolo !: le indicamos a Angular que el arreglo en algún momento recibira datos, que espere */
+
+
+  agregarPelicula(){
+    this.peliculas.push({
+      titulo: 'Inception',
+      fechaLanzamiento: new Date('2012-07-02'),
+      precio: 500.00,
+      poster: 'https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_%282010%29_theatrical_poster.jpg'
+    })
+  }
+
+  removerPelicula(pelicula : any){
+     const indice = this.peliculas.findIndex((peliculaActual : any) => peliculaActual.titulo === pelicula.titulo);
+     this.peliculas.splice(indice, 1)
+  }
+}
