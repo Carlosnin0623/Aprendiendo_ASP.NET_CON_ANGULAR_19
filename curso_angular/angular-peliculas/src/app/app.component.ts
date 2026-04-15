e49cc699b2fc74a25c256805fc02987020a1c375
Carlos Alberto Nin Queliz <128722564+Carlosnin0623@users.noreply.github.com>
2026-04-14 21:16:41 -0400

    Aprendiendo sobre el ruteo

--- curso_angular/angular-peliculas/src/app/app.component.ts
+++ curso_angular/angular-peliculas/src/app/app.component.ts
@@ -1,76 +1,27 @@
+<<<<<<< Updated upstream
 
 import { MenuComponent } from './compartidos/componentes/menu/menu.component';
 import { Component, OnInit } from '@angular/core';
 import { ListadoPeliculasComponent } from './peliculas/listado-peliculas/listado-peliculas.component';
 import { RatingComponent } from "./compartidos/componentes/rating/rating.component";
 import { MatButtonModule } from '@angular/material/button';
+=======
+import { Component } from '@angular/core';
+import {RouterOutlet } from '@angular/router';
+import { MenuComponent } from "./compartidos/componentes/menu/menu.component";
+>>>>>>> Stashed changes
 
 
 @Component({
   selector: 'app-root',
+<<<<<<< Updated upstream
   imports: [ListadoPeliculasComponent, MenuComponent, MatButtonModule],
+=======
+  imports: [RouterOutlet, MenuComponent],
+>>>>>>> Stashed changes
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit  {
-  ngOnInit(): void {
-    setTimeout(() => {
-      this.peliculasEnCines = [
-        {
-          titulo: 'Inside Out 2',
-          fechaLanzamiento: new Date(),
-          precio: 1400.99,
-          poster: 'https://upload.wikimedia.org/wikipedia/en/f/f7/Inside_Out_2_poster.jpg?20240514232832'
-        },
-        {
-          titulo: 'Moana 2',
-          fechaLanzamiento: new Date('2016-05-03'),
-          precio: 300.99,
-          poster: 'https://upload.wikimedia.org/wikipedia/en/7/73/Moana_2_poster.jpg'
-        },
-        {
-          titulo: 'Bad Boys: Ride or Die',
-          fechaLanzamiento: new Date('2016-05-03'),
-          precio: 300.99,
-          poster: 'https://upload.wikimedia.org/wikipedia/en/8/8b/Bad_Boys_Ride_or_Die_%282024%29_poster.jpg'
-        }
-      ];
-      this.peliculasProximosEstrenos = [
-          {
-          titulo: 'Deadpool & Wolverine',
-          fechaLanzamiento: new Date('2016-05-03'),
-          precio: 300.99,
-          poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/4c/Deadpool_%26_Wolverine_poster.jpg/250px-Deadpool_%26_Wolverine_poster.jpg'
-        },
-        {
-          titulo: 'Oppenheimer',
-          fechaLanzamiento: new Date('2016-05-03'),
-          precio: 300.99,
-          poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/4a/Oppenheimer_%28film%29.jpg/250px-Oppenheimer_%28film%29.jpg'
-        },
-        {
-          titulo: 'The Flash',
-          fechaLanzamiento: new Date('2016-05-03'),
-          precio: 300.99,
-          poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/e/ed/The_Flash_%28film%29_poster.jpg/250px-The_Flash_%28film%29_poster.jpg'
-        }
-
-      ]
-    },2000)
-  } 
+export class AppComponent {
  
-  peliculasEnCines!: any[];
-  peliculasProximosEstrenos!: any[];
-
-
-  procesarVoto(voto: number){
-    alert(`Calificación otorgada: ${voto}`);
-  }
-
-/*
-  clickBoton(){
-    alert('Me has clikqueado');
-  }
-
-*/
 }
--- curso_angular/angular-peliculas/src/app/peliculas/listado-peliculas/listado-peliculas.component.ts
+++ curso_angular/angular-peliculas/src/app/peliculas/listado-peliculas/listado-peliculas.component.ts
@@ -1,4 +1,3 @@
-import { CurrencyPipe, DatePipe, NgOptimizedImage, UpperCasePipe } from '@angular/common';
 import { Component, Input} from '@angular/core';
 import { ListadoGenericoComponent } from '../../compartidos/componentes/listado-generico/listado-generico.component';
 import { MatButtonModule } from '@angular/material/button';
@@ -7,7 +6,7 @@ import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-listado-peliculas',
-  imports: [DatePipe, UpperCasePipe, CurrencyPipe, NgOptimizedImage, ListadoGenericoComponent, MatButtonModule, MatIconModule],
+  imports: [ListadoGenericoComponent, MatButtonModule, MatIconModule],
   templateUrl: './listado-peliculas.component.html',
   styleUrl: './listado-peliculas.component.css'
 })
@@ -24,18 +23,4 @@ export class ListadoPeliculasComponent { /* Con OnInit le decimos lo que tiene q
   @Input({required: true})
   peliculas!: any[]; /* Con este simbolo !: le indicamos a Angular que el arreglo en algún momento recibira datos, que espere */
 
-
-  agregarPelicula(){
-    this.peliculas.push({
-      titulo: 'Inception',
-      fechaLanzamiento: new Date('2012-07-02'),
-      precio: 500.00,
-      poster: 'https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_%282010%29_theatrical_poster.jpg'
-    })
-  }
-
-  removerPelicula(pelicula : any){
-     const indice = this.peliculas.findIndex((peliculaActual : any) => peliculaActual.titulo === pelicula.titulo);
-     this.peliculas.splice(indice, 1)
-  }
 }
