92c4a6e841c47d06878c94580bc3671dc2cc39b9
Carlos Alberto Nin Queliz <128722564+Carlosnin0623@users.noreply.github.com>
2026-04-16 11:39:28 -0400

    actualizar componentes

--- curso_angular/angular-peliculas/src/app/app.component.ts
+++ curso_angular/angular-peliculas/src/app/app.component.ts
@@ -1,24 +1,15 @@
-<<<<<<< Updated upstream
-
-import { MenuComponent } from './compartidos/componentes/menu/menu.component';
-import { Component, OnInit } from '@angular/core';
 import { ListadoPeliculasComponent } from './peliculas/listado-peliculas/listado-peliculas.component';
 import { RatingComponent } from "./compartidos/componentes/rating/rating.component";
 import { MatButtonModule } from '@angular/material/button';
-=======
 import { Component } from '@angular/core';
 import {RouterOutlet } from '@angular/router';
 import { MenuComponent } from "./compartidos/componentes/menu/menu.component";
->>>>>>> Stashed changes
+
 
 
 @Component({
   selector: 'app-root',
-<<<<<<< Updated upstream
-  imports: [ListadoPeliculasComponent, MenuComponent, MatButtonModule],
-=======
   imports: [RouterOutlet, MenuComponent],
->>>>>>> Stashed changes
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
