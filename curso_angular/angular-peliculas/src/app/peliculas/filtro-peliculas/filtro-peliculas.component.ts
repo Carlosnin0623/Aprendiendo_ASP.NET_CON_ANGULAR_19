0a4a0a5139b240dbd28f9742dd8739338f59ff52
Carlos Alberto Nin Queliz <128722564+Carlosnin0623@users.noreply.github.com>
2026-04-28 17:03:18 -0400

    Peliculas Filtro terminado

--- curso_angular/angular-peliculas/src/app/peliculas/filtro-peliculas/filtro-peliculas.component.ts
+++ curso_angular/angular-peliculas/src/app/peliculas/filtro-peliculas/filtro-peliculas.component.ts
@@ -1,11 +1,12 @@
 import { MatInputModule } from '@angular/material/input';
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import {MatSelectModule} from '@angular/material/select';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import { ListadoPeliculasComponent } from "../listado-peliculas/listado-peliculas.component";
+import { FiltroPeliculas } from './filtro-peliculas';
 
 @Component({
   selector: 'app-filtro-peliculas',
@@ -13,8 +14,42 @@ import { ListadoPeliculasComponent } from "../listado-peliculas/listado-pelicula
   templateUrl: './filtro-peliculas.component.html',
   styleUrl: './filtro-peliculas.component.css'
 })
-export class FiltroPeliculasComponent {
+export class FiltroPeliculasComponent implements OnInit {
+  ngOnInit(): void {
+    /* valueChanges.subscribe esto permite reaccionar a los cambios en los valores de los campos de los formulario */
+    this.form.valueChanges.subscribe(valores => {
+        this.peliculas = this.peliculasOriginal;
+        this.buscarPeliculas(valores as FiltroPeliculas);
+    });
 
+  }
+
+  buscarPeliculas(valores: FiltroPeliculas){
+    if(valores.titulo){
+      this.peliculas = this.peliculas.filter(pelicula => pelicula.titulo.indexOf(valores.titulo) !== -1);
+    }
+
+    if(valores.generoId !== 0){
+      this.peliculas = this.peliculas.filter(pelicula => pelicula.generos.indexOf(valores.generoId) !== -1);
+    }
+
+    if(valores.proximosEstrenos){
+      this.peliculas = this.peliculas.filter(pelicula => pelicula.proximosEstrenos);
+    }
+
+     if(valores.enCines){
+      this.peliculas = this.peliculas.filter(pelicula => pelicula.enCines);
+    }
+  }
+
+  limpiar(){
+    this.form.patchValue({
+      titulo: '',
+      generoId:0,
+      proximosEstrenos: false,
+      enCines: false
+    });
+  }
 
   private formBuilder = inject(FormBuilder);
 
@@ -31,44 +66,66 @@ export class FiltroPeliculasComponent {
     {id:3, nombre:"Comedia"}
   ];
 
-  peliculas = [
+  peliculasOriginal = [
         {
           titulo: 'Inside Out 2',
           fechaLanzamiento: new Date(),
           precio: 1400.99,
-          poster: 'https://upload.wikimedia.org/wikipedia/en/f/f7/Inside_Out_2_poster.jpg?20240514232832'
+          poster: 'https://upload.wikimedia.org/wikipedia/en/f/f7/Inside_Out_2_poster.jpg?20240514232832',
+          generos:[1,2,3],
+          enCines: true,
+          proximosEstrenos: false
         },
         {
           titulo: 'Moana 2',
           fechaLanzamiento: new Date('2016-05-03'),
           precio: 300.99,
-          poster: 'https://upload.wikimedia.org/wikipedia/en/7/73/Moana_2_poster.jpg'
+          poster: 'https://upload.wikimedia.org/wikipedia/en/7/73/Moana_2_poster.jpg',
+          generos:[3],
+          enCines: false,
+          proximosEstrenos: true
         },
 
         {
           titulo: 'Bad Boys: Ride or Die',
           fechaLanzamiento: new Date('2016-05-03'),
           precio: 300.99,
-          poster: 'https://upload.wikimedia.org/wikipedia/en/8/8b/Bad_Boys_Ride_or_Die_%282024%29_poster.jpg'
+          poster: 'https://upload.wikimedia.org/wikipedia/en/8/8b/Bad_Boys_Ride_or_Die_%282024%29_poster.jpg',
+          generos:[1],
+          enCines: false,
+          proximosEstrenos: false
         },
 
         {
           titulo: 'Deadpool & Wolverine',
           fechaLanzamiento: new Date('2016-05-03'),
           precio: 300.99,
-          poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/4c/Deadpool_%26_Wolverine_poster.jpg/250px-Deadpool_%26_Wolverine_poster.jpg'
+          poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/4c/Deadpool_%26_Wolverine_poster.jpg/250px-Deadpool_%26_Wolverine_poster.jpg',
+          generos:[],
+          enCines: true,
+          proximosEstrenos: false
         },
         {
           titulo: 'Oppenheimer',
           fechaLanzamiento: new Date('2016-05-03'),
           precio: 300.99,
-          poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/4a/Oppenheimer_%28film%29.jpg/250px-Oppenheimer_%28film%29.jpg'
+          poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/4a/Oppenheimer_%28film%29.jpg/250px-Oppenheimer_%28film%29.jpg',
+          generos:[1,3],
+          enCines: false,
+          proximosEstrenos: true
         },
         {
           titulo: 'The Flash',
           fechaLanzamiento: new Date('2016-05-03'),
           precio: 300.99,
-          poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/e/ed/The_Flash_%28film%29_poster.jpg/250px-The_Flash_%28film%29_poster.jpg'
+          poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/e/ed/The_Flash_%28film%29_poster.jpg/250px-The_Flash_%28film%29_poster.jpg',
+          generos:[2,3],
+          enCines: false,
+          proximosEstrenos: false
         }
       ];
+
+   peliculas = this.peliculasOriginal; 
 }
+
+
