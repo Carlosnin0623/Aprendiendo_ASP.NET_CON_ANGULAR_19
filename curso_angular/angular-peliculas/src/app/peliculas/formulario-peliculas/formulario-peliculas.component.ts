553828e8ecf09987de78bac2f782372ddc01036f
Carlos Alberto Nin Queliz <128722564+Carlosnin0623@users.noreply.github.com>
2026-05-04 18:31:27 -0400

    Trabajando en el componente Pelicula

---
+++ curso_angular/angular-peliculas/src/app/peliculas/formulario-peliculas/formulario-peliculas.component.ts
@@ -0,0 +1,84 @@
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { Component, EventEmitter, inject, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { RouterLink } from '@angular/router';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { InputImgComponent } from '../../compartidos/componentes/input-img/input-img.component';
+import { PeliculaCreacionDTO, PeliculaDTO } from '../peliculas';
+import moment from 'moment';
+import {primeraLetraMayuscula } from '../../compartidos/funciones/validaciones';
+
+@Component({
+  selector: 'app-formulario-peliculas',
+  imports: [MatFormFieldModule, ReactiveFormsModule, MatInputModule, MatButtonModule, RouterLink, MatDatepickerModule, InputImgComponent],
+  templateUrl: './formulario-peliculas.component.html',
+  styleUrl: './formulario-peliculas.component.css'
+})
+export class FormularioPeliculasComponent implements OnInit {
+ngOnInit(): void {
+  if(this.modelo !== undefined){
+    this.form.patchValue(this.modelo);
+  }
+}
+
+
+@Input()
+modelo?: PeliculaDTO;
+
+@Output()
+posteoFormulario = new EventEmitter<PeliculaCreacionDTO>();
+
+
+private formBuilder = inject(FormBuilder);
+
+form = this.formBuilder.group({
+  titulo: ['', {validators: [Validators.required, primeraLetraMayuscula()]}],
+  fechaLanzamiento: new FormControl<Date | null>(null, {validators: [Validators.required]}),
+  trailer: '',
+  poster: new FormControl<File | string | null>(null)
+});
+
+archivoSeleccionado(file: File){
+  this.form.controls.poster.setValue(file);
+}
+
+guardarCambios(){
+  if(!this.form.valid){
+     return;
+  }
+
+  const pelicula = this.form.value as PeliculaCreacionDTO;
+
+  pelicula.fechaLanzamiento = moment(pelicula.fechaLanzamiento).toDate();
+
+  this.posteoFormulario.emit(pelicula);
+}
+
+
+obtenerErrorCampoTitulo(): string{
+  let campo = this.form.controls.titulo;
+
+  if(campo.hasError('required')){
+    return 'El campo titulo es requerido';
+  }
+
+  if (campo.hasError('primeraLetraMayuscula')) {
+      return campo.getError('primeraLetraMayuscula').mensaje;
+  }
+
+  return '';
+}
+
+obtenerErrorCampoFechaLanzamiento(): string{
+  let campo = this.form.controls.fechaLanzamiento;
+
+  if (campo.hasError('required')) {
+      return "El campo fecha lanzamiento es requerido";
+  }
+  
+  return '';
+}
+
+}
